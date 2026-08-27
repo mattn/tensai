96d4b610b725bf73f243a5759120927ab58e4f1a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-08-27 15:57:27 +0900

    Bump up version to 0.0.9

--- cmd/tensai/main.go
+++ cmd/tensai/main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/mattn/tensai/internal/llm"
 )
 
-const version = "0.0.8"
+const version = "0.0.9"
 
 // revision is stamped by the release build (-X main.revision=...).
 var revision = "HEAD"
