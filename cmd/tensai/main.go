345611e5361f04a19bce9aa5a03eb8a529b14a38
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-09-25 16:11:01 +0900

    Bump up version to 0.0.32

--- cmd/tensai/main.go
+++ cmd/tensai/main.go
@@ -33,7 +33,7 @@ import (
 	"github.com/mattn/tensai/internal/simd"
 )
 
-const version = "0.0.31"
+const version = "0.0.32"
 
 // revision is stamped by the release build (-X main.revision=...).
 var revision = "HEAD"
